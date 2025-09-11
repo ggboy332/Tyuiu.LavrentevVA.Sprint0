ea4ea29a2af4a916a8ac00909bb17e81ea1d2b6b
dimka <dimka556.dim4@gmail.com>
2025-09-11 20:05:17 +0500

    Task3

--- Tyuiu.LavrentevVA.Sprint0.Task3.V0/Program.cs
+++ Tyuiu.LavrentevVA.Sprint0.Task3.V0/Program.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace Tyuiu.LavrentevVA.Sprint0.Task0.V0
+using Tyuiu.LavrentevVA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.LavrentevVA.Sprint0.Task3.V0
 {
     class Program
     {
         static void Main(string[] args)
-        {  
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
         }
     }
 }
