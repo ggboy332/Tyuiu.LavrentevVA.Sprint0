a32b1ffeaeca2e27e488fd00062fb49e3161ab2f
dimka <dimka556.dim4@gmail.com>
2025-09-13 16:24:38 +0500

    Task4

---
+++ Tyuiu.LavrentevVA.Sprin0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LavrenvVA.Sprin0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LavrenvVA.Sprin0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("205f2db3-5975-48e1-837d-1152a4f8008c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.LavrentevVA.Sprin0.Task4.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LavrenvVA.Sprin0.Task4.V0.Lib;
+
+namespace Tyuiu.LavrenvVA.Sprin0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Division(1, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
